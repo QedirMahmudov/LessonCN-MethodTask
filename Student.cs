430645d4640fd18b6460ccbcadfb6e1748bd0f69
Limon <Limon@LAPTOP-SMPG3CJI>
2024-07-08 23:23:25 +0400

    Add project files.

---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonCN_Practice
+{
+    public class Student
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = "Qedir"; }
+        }
+    }
+}
